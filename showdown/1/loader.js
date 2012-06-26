7c1c8384e546b7c093b7416588976aa7080ef6ae
Brandon Holtsclaw <me@brandonholtsclaw.com>
2012-06-26 16:24:04 -0500

    remove anon function that isnt working ?

--- showdown/1/loader.js
+++ showdown/1/loader.js
@@ -1,19 +1,18 @@
 //<!-- Ubuntu App Showdown Widget http://www.brandonholtsclaw.com/. Copyright 2012 Brandon Holtsclaw. -->
 $.ready(function() {
   $("<link>", {rel:"stylesheet", type:"text/css", href:"http://bholtsclaw.github.com/showdown/1/full.min.css?v=42"}).appendTo("head");
-  $.getScript('http://bholtsclaw.github.com/showdown/1/full.min.js?ver=42', function() {
+  $.getScript('http://bholtsclaw.github.com/showdown/1/full.min.js?ver=42');
     var c=new Date(2012, 6, 9, 23, 59, 0, 0);
     var b=new Date(2012, 5, 18, 0, 0, 0, 1);
   	var a=new luxCountdown ({
-  	  title: "<img id='app-showdown-widget-logo' src='https://developer.ubuntu.com/wp-content/themes/wordpress-theme-ubuntudeveloper/img/logo-ubuntu.png'><span class='app_showdown_widget_header'><span class='ufont'>App Developer</span></span> <div id='app_showdown_widget_action_title'><span class='ufont'><em>Showdown !</em></span></div>",
-      text: "<br><hr size='1' noshadow shadow='none' color='#ccc' border-color='#ccc' style='width: 95%;padding: -25px 0 0 0;' /><div class='copy'><span class='ufont'>ubuntu's</span> <a href='//developer.ubuntu.com'>Developer Community</a> is growing. Come Check out the new <a href='//developer.ubuntu.com/'>Ubuntu Dev Portal</a>. We send twitter status updates <a href='//twitter.com/ubuntuappdev'>@ubuntuappdev</a> too !</div><hr size='1' noshadow shadow='none' color='#ccc' border-color='#ccc' style='width: 95%;padding: -25px 0 0 0;'/></div>",
-      start: b,
-      end: c,
-      startText: '',
-      endText: ''
-    });
-    $('#showdown-widget-1').html(a.getCountdown());
+    title: "<img id='app-showdown-widget-logo' src='https://developer.ubuntu.com/wp-content/themes/wordpress-theme-ubuntudeveloper/img/logo-ubuntu.png'><span class='app_showdown_widget_header'><span class='ufont'>App Developer</span></span> <div id='app_showdown_widget_action_title'><span class='ufont'><em>Showdown !</em></span></div>",
+    text: "<br><hr size='1' noshadow shadow='none' color='#ccc' border-color='#ccc' style='width: 95%;padding: -25px 0 0 0;' /><div class='copy'><span class='ufont'>ubuntu's</span> <a href='//developer.ubuntu.com'>Developer Community</a> is growing. Come Check out the new <a href='//developer.ubuntu.com/'>Ubuntu Dev Portal</a>. We send twitter status updates <a href='//twitter.com/ubuntuappdev'>@ubuntuappdev</a> too !</div><hr size='1' noshadow shadow='none' color='#ccc' border-color='#ccc' style='width: 95%;padding: -25px 0 0 0;'/></div>",
+    start: b,
+    end: c,
+    startText: '',
+    endText: ''
   });
+  $('#showdown-widget-1')appendTo(a.getCountdown());
 });
 
 //$("<link>", {rel:"stylesheet", type:"text/css", href:"http://bholtsclaw.github.com/showdown/1/luxCountdown.css?v=42"}).appendTo("head");
