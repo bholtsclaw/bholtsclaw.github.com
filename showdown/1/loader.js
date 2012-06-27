0ae565b375d02b06f81ebdfb9231069fc107265f
Brandon Holtsclaw <me@brandonholtsclaw.com>
2012-06-26 20:35:59 -0500

    minor side update to the copy/paste font and adding links to the header going to developer.ubuntu.com/showdown

--- showdown/1/loader.js
+++ showdown/1/loader.js
@@ -85,7 +85,7 @@
         var c=new Date(2012, 6, 9, 23, 59, 0, 0);
         var b=new Date(2012, 5, 18, 0, 0, 0, 1);
   	    var a=new luxCountdown ({
-        title: "<img id='app-showdown-widget-logo' src='https://developer.ubuntu.com/wp-content/themes/wordpress-theme-ubuntudeveloper/img/logo-ubuntu.png'><span class='app_showdown_widget_header'><span class='ufont'>App Developer</span></span> <div id='app_showdown_widget_action_title'><span class='ufont'><em>Showdown !</em></span></div>",
+        title: "<a href='http://developer.ubuntu.com/showdown?from=widget' _target='_top'><img id='app-showdown-widget-logo' src='https://developer.ubuntu.com/wp-content/themes/wordpress-theme-ubuntudeveloper/img/logo-ubuntu.png'><span class='app_showdown_widget_header'><span class='ufont'>App Developer</span></span> <div id='app_showdown_widget_action_title'><span class='ufont'><em>Showdown !</em></span></div></a>",
         text: "<br><hr size='1' noshadow shadow='none' color='#ccc' border-color='#ccc' style='width: 95%;padding: -25px 0 0 0;' /><div class='copy'><span class='ufont'>ubuntu's</span> <a href='//developer.ubuntu.com'>Developer Community</a> is growing. Come Check out the new <a href='//developer.ubuntu.com/'>Ubuntu Dev Portal</a>. We send twitter status updates <a href='//twitter.com/ubuntuappdev'>@ubuntuappdev</a> too !</div><hr size='1' noshadow shadow='none' color='#ccc' border-color='#ccc' style='width: 95%;padding: -25px 0 0 0;'/></div>",
         start: b,
         end: c,
