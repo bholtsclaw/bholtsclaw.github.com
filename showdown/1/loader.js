d8dd6bb2cc79701ae2b596a2d911c76f36dbd543
Brandon Holtsclaw <me@brandonholtsclaw.com>
2012-06-27 18:44:09 -0500

    slight cleanups

--- showdown/1/loader.js
+++ showdown/1/loader.js
@@ -10,9 +10,7 @@
 /// -- @About me:
 /// -- I make cool things, mostly on the Web, I do it by Consulting Freelance so if you have a 
 /// -- unique problem you'd like to confidentially go over with me just use any of my contacts.
-/// -- My website @ http://www.brandonholtsclaw.com ,Referals for stuff I use & Tips are always cool.
-/// -- Once upon a time I would try to keep 40 to 100 nodes up and consistant as well as uptodate and 
-/// -- custom build env too, overwelmed! Now I turn on and off that many almost daily on jujuo EC2!!.
+/// -- My website @ http://www.brandonholtsclaw.com , Referals for stuff I use.
 /// ------>
 
 (function() {
@@ -41,14 +39,12 @@
   function main() { 
     ubjQuery(document).ready(function($) {
       $("<link>", {rel:"stylesheet", type:"text/css", href:"http://bholtsclaw.github.com/showdown/1/full.min.css?v=42"}).appendTo("head");
-      //$.getScript('http://bholtsclaw.github.com/showdown/1/luxUtilities.min.js?ver=42');
-      //$.getScript('http://bholtsclaw.github.com/showdown/1/luxCountdown.min.js?ver=42');
       $.getScript('http://bholtsclaw.github.com/showdown/1/full.js?v=42', function(){
         var c=new Date(2012, 6, 9, 23, 59, 0, 0);
         var b=new Date(2012, 5, 18, 0, 0, 0, 1);
   	    var a=new luxCountdown ({
         title: "<a href='http://developer.ubuntu.com/showdown?from=widget' _target='_top'><img id='app-showdown-widget-logo' src='https://developer.ubuntu.com/wp-content/themes/wordpress-theme-ubuntudeveloper/img/logo-ubuntu.png'><span class='app_showdown_widget_header'><span class='ufont'>App Developer</span></span> <div id='app_showdown_widget_action_title'><span class='ufont'><em>Showdown !</em></span></div></a>",
-        text: "<br><hr size='1' noshadow shadow='none' color='#ccc' border-color='#ccc' style='width: 95%;padding: -25px 0 0 0;' /><div class='copy'><span class='ufont'>ubuntu's</span> <a href='//developer.ubuntu.com'>Developer Community</a> is growing. Come Check out the new <a href='//developer.ubuntu.com/'>Ubuntu Dev Portal</a>. We send twitter status updates <a href='//twitter.com/ubuntuappdev'>@ubuntuappdev</a> too !</div><hr size='1' noshadow shadow='none' color='#ccc' border-color='#ccc' style='width: 95%;padding: -25px 0 0 0;'/></div>",
+        text: "<br><hr size='1' noshadow shadow='none' color='#ccc' border-color='#ccc' style='width: 95%;padding: -25px 0 0 0;' /><div class='copy'><span class='ufont'>ubuntu's</span> <a href='//developer.ubuntu.com'>Developer Community</a> is growing. Come Check out the new <a href='//developer.ubuntu.com/'>Ubuntu Dev Portal</a>. We send twitter status updates <a href='//twitter.com/ubuntuappdev'>@ubuntuappdev</a></div><hr size='1' noshadow shadow='none' color='#ccc' border-color='#ccc' style='width: 95%;padding: -25px 0 0 0;'/></div>",
         start: b,
         end: c,
         startText: '',
